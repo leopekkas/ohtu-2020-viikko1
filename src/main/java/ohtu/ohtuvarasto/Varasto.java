ff0feca4b129fd96e72d17d17862314bb2098f37
Leopekka Saraste <leopekkas@gmail.com>
2020-10-26 19:13:32 +0200

    Testikommentti codecovia varten

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -1,6 +1,7 @@
 package ohtu.ohtuvarasto;
 
 public class Varasto {
+    // Testikommentti
 
     // --- piilotettu tietorakenteen toteutus: ---
     private double tilavuus;  // paljonko varastoon mahtuu,  > 0
