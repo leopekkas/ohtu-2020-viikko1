3604c4ccaffc3493caf6a06a5ad5446fb6a11779
Leopekka Saraste <leopekkas@gmail.com>
2020-11-02 12:36:22 +0200

    Add: Checkstyle-methods withno violations

--- src/main/java/paaohjelma/Main.java
+++ src/main/java/paaohjelma/Main.java
@@ -6,6 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
 
+    }
+    
+    /*
         Varasto mehua = new Varasto(100.0);
         Varasto olutta = new Varasto(100.0, 20.2);
 
@@ -56,5 +59,6 @@ public class Main {
         saatiin = mehua.otaVarastosta(-32.9);
         System.out.println("saatiin " + saatiin);
         System.out.println("Mehuvarasto: " + mehua);
-    }
+        */
+
 }
